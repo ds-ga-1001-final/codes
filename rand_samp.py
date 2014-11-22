5c7d99c04dac3313d2c6a478721cfe41904f34d4
EMIAOZANG <christopher.w.lu@gmail.com>
2014-11-22 03:10:36 -0500

    sample_train data size from 36400-364000, generated by 'rand_samp.py' the data comes from the first 1/6 part of the entire dataset, the time span is from OCT 21 12am to OCT 22 4am, this sample might be severely biased, but let's just try out the models using these datasets, i ll try to make a better sample set in a day or two

--- rand_samp.py
+++ rand_samp.py
@@ -14,20 +14,37 @@ def randomSampleDF(df,sampsize):
     indices = random.sample(df.index,sampsize)
     return df.ix[indices]
 
+def clearEmptyLines(filename):
+    command_str = 'sed /^$/d '+ filename #clearing empty lines 
+    print command_str
+    os.system(command_str)
+
 def samplingDataset(dir,sampsize):
-    sample_file = open('sample_train.csv','a')  #target file for sample
+    sample_file_name = 'sample_train_{}.csv'.format(sampsize*182)
+    sample_file = open(sample_file_name,'a')  #target file for sample
+    is_first = True
     for filename in os.listdir(dir):
         if filename.endswith('.csv'): 
-            unsamped_df = pd.read_csv(filename)       
+            print filename + '\n'
+            unsamped_df = pd.read_csv(os.path.join(dir,filename))       
             samp_df = randomSampleDF(unsamped_df, sampsize)
-            print >> sample_file, samp_df
+            print samp_df
+#             print >> sample_file, samp_df
+            if is_first:
+                samp_df.to_csv(sample_file_name,index=False,mode='a',header=True)
+                is_first = False
+            else:
+                samp_df.to_csv(sample_file_name,index=False,mode='a',header=False)
     sample_file.close()
+#     clearEmptyLines(sample_file_name)
+
     
 
 def main():
-    dirname = raw_input('please input dirname:\n')
+#     dirname = raw_input('please input dirname:\n')
     samp_size = input('please input sample size:\n')
-    samplingDataset(dirname,samp_size)  #sample 10000 records from each documents
+#     samplingDataset(dirname,samp_size)  #sample 10000 records from each documents
+    samplingDataset('/users/luchristopher/documents/ctr_data_cleaned/parsed_train', samp_size)
     
     
 if __name__ == '__main__':
